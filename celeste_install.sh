7064e5f80982025c224e11f872dca1c2ad6b663f
Garrett <gmesmer@comcast.net>
2020-08-13 22:32:36 -0400

    v1.04.4 release

--- celeste_install.sh
+++ celeste_install.sh
@@ -6,10 +6,10 @@ echo
 sudo apt install --assume-yes libc6 libsdl1.2debian libsdl-mixer1.2 libc6 libasound2 libc6 libpulse0 libx11-6 libxext6 libcaca0 libc6 libmikmod3 libfluidsynth1 libvorbisfile3 libflac8 libmad0 libpulse0 libdbus-1-3 libxcb1 libslang2 libncursesw5 libtinfo5 zlib1g libsdl2-2.0-0 libopenal1 libglib2.0-0 libjack-jackd2-0 libsndfile1 libreadline7 libvorbis0a libogg0 libsystemd0 libwrap0 libasyncns0 libc6 libxau6 libxdmcp6 libsndio6.1 libpcre3 libstdc++6 libgcc1 libvorbisenc2 liblzma5 liblz4-1 libgcrypt20 libc6 libbsd0 libgpg-error0
 echo
 echo "Downloading the game"
-wget https://github.com/theofficialgman/ccleste/archive/v1.04.3.zip
-unzip v1.04.3.zip
-rm v1.04.3.zip
-mv ccleste-1.04.3 celeste
+wget https://github.com/theofficialgman/ccleste/archive/v1.04.4.zip
+unzip v1.04.4.zip
+rm v1.04.4.zip
+mv ccleste-1.04.4 celeste
 cd celeste
 chmod 777 celeste-sdl12
 echo
