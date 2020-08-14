c51727940841570bcee6ec87a8c3287991ef3617
Garrett <gmesmer@comcast.net>
2020-08-13 21:05:38 -0400

    assume yes for dependencies

--- celeste_install.sh
+++ celeste_install.sh
@@ -3,7 +3,7 @@ cd
 echo "Celeste Pico 8 script started!"
 echo "Installing Dependencies"
 echo
-sudo apt install libc6 libsdl1.2debian libsdl-mixer1.2 libc6 libasound2 libc6 libpulse0 libx11-6 libxext6 libcaca0 libc6 libmikmod3 libfluidsynth1 libvorbisfile3 libflac8 libmad0 libpulse0 libdbus-1-3 libxcb1 libslang2 libncursesw5 libtinfo5 zlib1g libsdl2-2.0-0 libopenal1 libglib2.0-0 libjack-jackd2-0 libsndfile1 libreadline7 libvorbis0a libogg0 libsystemd0 libwrap0 libasyncns0 libc6 libxau6 libxdmcp6 libsndio6.1 libpcre3 libstdc++6 libgcc1 libvorbisenc2 liblzma5 liblz4-1 libgcrypt20 libc6 libbsd0 libgpg-error0
+sudo apt install --assume-yes libc6 libsdl1.2debian libsdl-mixer1.2 libc6 libasound2 libc6 libpulse0 libx11-6 libxext6 libcaca0 libc6 libmikmod3 libfluidsynth1 libvorbisfile3 libflac8 libmad0 libpulse0 libdbus-1-3 libxcb1 libslang2 libncursesw5 libtinfo5 zlib1g libsdl2-2.0-0 libopenal1 libglib2.0-0 libjack-jackd2-0 libsndfile1 libreadline7 libvorbis0a libogg0 libsystemd0 libwrap0 libasyncns0 libc6 libxau6 libxdmcp6 libsndio6.1 libpcre3 libstdc++6 libgcc1 libvorbisenc2 liblzma5 liblz4-1 libgcrypt20 libc6 libbsd0 libgpg-error0
 echo
 echo "Downloading the game"
 wget https://github.com/theofficialgman/ccleste/archive/v1.04.3.zip
